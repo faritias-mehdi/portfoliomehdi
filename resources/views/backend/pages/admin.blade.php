3b2395ccd9922ebf5573285688c6c21db8c877b5
faritias-mehdi <faritiasmehdi17@gmail.com>
2023-09-11 12:35:23 +0100

    portfoliomedi

---
+++ resources/views/backend/pages/admin.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.indexbackend")
+@section("content")
+@foreach ($homes as $home )
+@include("backend.partials.admin.home")
+@endforeach
+@foreach ($abouts as $about)
+    @include("backend.partials.admin.aboutback")
+@endforeach
+@foreach ($skills as $skill)
+    @include("backend.partials.admin.skillback")
+@endforeach
+<a href={{route("home.index")}}>page front</a>
+@endsection
